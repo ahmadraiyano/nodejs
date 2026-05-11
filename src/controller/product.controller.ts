c1f876e4a6ac55a343f66f0cb5db26d500c7f6b9
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-05-11 17:20:42 +0600

    created service, local db and retrieved data

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,8 +1,22 @@
-import type { IncomingMessage, ServerResponse } from "node:http";
+import type { IncomingMessage, ServerResponse } from "http";
+import { readProduct } from "../service/product.service";
 
-export const productControll = (req: IncomingMessage, res: ServerResponse) => {
+export const productController = (
+  req: IncomingMessage,
+  res: ServerResponse,
+) => {
   const url = req.url;
   const method = req.method;
-  res.writeHead(200, { "content-type": "application/json" });
-  res.end(JSON.stringify({ message: "This is products route from controller" }));
+  // const products = [{ id: 1, name: "phone", description: "redmi note 12", price : 14000 }];
+
+  if (url === "/products" && method === "GET") {
+    const products = readProduct()
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "products data retrieved successfully",
+        data: products,
+      }),
+    );
+  }
 };
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,5 +1,5 @@
-import type { IncomingMessage, ServerResponse } from "node:http";
-import { productControll } from "../controller/product.controller";
+import type { IncomingMessage, ServerResponse } from "http";
+import { productController } from "../controller/product.controller";
 
 export const handleRoute = (req: IncomingMessage, res: ServerResponse) => {
   const url = req.url;
@@ -8,8 +8,7 @@ export const handleRoute = (req: IncomingMessage, res: ServerResponse) => {
     res.writeHead(200, { "content-type": "application/json" });
     res.end(JSON.stringify({ message: "This is root route" }));
   } else if (url?.startsWith("/products")) {
-    productControll(req,res)
-    
+    productController(req, res);
   } else {
     res.writeHead(404, { "content-type": "application/json" });
     res.end(JSON.stringify({ message: "route not found" }));
