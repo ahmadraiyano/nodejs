bc88f31ef64d60dddbcb6b3b31e34a2faedd2483
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-05-11 17:58:03 +0600

    created dynamic route for single product

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
+import type { IProduct } from "../types/product.type";
 
 export const productController = (
   req: IncomingMessage,
@@ -8,6 +9,8 @@ export const productController = (
   const url = req.url;
   const method = req.method;
   // const products = [{ id: 1, name: "phone", description: "redmi note 12", price : 14000 }];
+  const splitUrl = url?.split("/")
+  const id = splitUrl && splitUrl[1] === "products" ? Number(splitUrl[2]) : null
 
   if (url === "/products" && method === "GET") {
     const products = readProduct()
@@ -16,6 +19,16 @@ export const productController = (
       JSON.stringify({
         message: "products data retrieved successfully",
         data: products,
+      }),
+    );
+  } else if (method === "GET" && url !== null){
+    const products = readProduct()
+    const product = products.find((p: IProduct)=>p.id === id)
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "products data retrieved successfully",
+        data: product,
       }),
     );
   }
