a9516193f745a7607a8236ce24247d5af1f85cdd
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-05-11 18:43:13 +0600

    created utility for body parse

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,19 +1,21 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
 import type { IProduct } from "../types/product.type";
+import { parseBody } from "../utility/parseBody";
 
-export const productController = (
+export const productController = async (
   req: IncomingMessage,
   res: ServerResponse,
 ) => {
   const url = req.url;
   const method = req.method;
   // const products = [{ id: 1, name: "phone", description: "redmi note 12", price : 14000 }];
-  const splitUrl = url?.split("/")
-  const id = splitUrl && splitUrl[1] === "products" ? Number(splitUrl[2]) : null
+  const splitUrl = url?.split("/");
+  const id =
+    splitUrl && splitUrl[1] === "products" ? Number(splitUrl[2]) : null;
 
   if (url === "/products" && method === "GET") {
-    const products = readProduct()
+    const products = readProduct();
     res.writeHead(200, { "content-type": "application/json" });
     res.end(
       JSON.stringify({
@@ -21,14 +23,24 @@ export const productController = (
         data: products,
       }),
     );
-  } else if (method === "GET" && url !== null){
-    const products = readProduct()
-    const product = products.find((p: IProduct)=>p.id === id)
+  } else if (method === "GET" && url !== null) {
+    const products = readProduct();
+    const product = products.find((p: IProduct) => p.id === id);
     res.writeHead(200, { "content-type": "application/json" });
     res.end(
       JSON.stringify({
         message: "products data retrieved successfully",
         data: product,
+      }),
+    );
+  } else if(method === "POST" && url === "/products"){
+    const body = await parseBody(req)
+    console.log("Body", body);
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "products data created successfully",
+        // data: product,
       }),
     );
   }
