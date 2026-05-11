6147160be08e4b310a0b8f838aea7a7dff22749f
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-05-12 00:15:08 +0600

    learned data write method

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,5 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "http";
-import { readProduct } from "../service/product.service";
+import { insertProduct, readProduct } from "../service/product.service";
 import type { IProduct } from "../types/product.type";
 import { parseBody } from "../utility/parseBody";
 
@@ -35,12 +35,20 @@ export const productController = async (
     );
   } else if(method === "POST" && url === "/products"){
     const body = await parseBody(req)
-    console.log("Body", body);
+    // console.log("Body", body);
+    const products = readProduct();
+    const newProduct = {
+      id: Date.now(),
+      ...body
+    }
+    products.push(newProduct)
+    // console.log(products)
+    insertProduct(products)
     res.writeHead(200, { "content-type": "application/json" });
     res.end(
       JSON.stringify({
         message: "products data created successfully",
-        // data: product,
+        data: newProduct,
       }),
     );
   }
--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -2,7 +2,14 @@ import fs from "fs"
 import path from "path"
 
 const filePath = path.join(process.cwd(),"./src/database/db.json")
+
+
 export const readProduct = () => {
 const products = fs.readFileSync(filePath, "utf-8")
 return JSON.parse(products)
+}
+
+export const insertProduct = (payload: any) =>{
+    console.log(payload);
+fs.writeFileSync(filePath, JSON.stringify(payload))
 }
